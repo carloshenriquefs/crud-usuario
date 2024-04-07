7d630a26230b38acbe3e7d38eb9ecaa5a41a898a
Carlos Henrique <ichfs9108@gmail.com>
2024-04-07 03:19:38 -0300

    feat: Convertendo dados, add constants e exception

---
+++ usuario/src/main/java/com/javanauta/usuario/constants/Constants.java
@@ -0,0 +1,8 @@
+package com.javanauta.usuario.constants;
+
+public class Constants {
+    public static final String DADOS_DO_USUARIO_SAO_OBRIGATORIOS = "Os dados do usuário são obrigatórios.";
+    public static final String ERRO_AO_BUSCAR_DADOS_DE_USUARIO = "Erro ao buscar dados de usuário.";
+    public static final String USUARIO_NAO_ENCONTRADO = "Usuário não encontrado.";
+    public static final String ERRO_AO_GRAVAR_DADOS_DO_USUARIO = "Erro ao gravar dados do usuário.";
+}
